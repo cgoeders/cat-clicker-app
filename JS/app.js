3aa217ba95286edeadce755866a801579c683735
Christina Goeders <xgoeders@gmail.com>
2015-10-05 23:55:20 -0700

    Added scratching alert.

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,17 @@
 var num_clicks = 0;
 var $click_counter = $("#click-int");
 
+var will_scratch = function(count) {
+	if (count % 5 === 0) {
+		alert("*scratch*");
+	}
+};
+
+
 $("#cat-img").click(function(e) {
 	num_clicks++;
 	console.log(num_clicks);
 	console.log("string " + num_clicks.toString());
 	$click_counter.text(num_clicks.toString());
+	will_scratch(num_clicks);
 });
